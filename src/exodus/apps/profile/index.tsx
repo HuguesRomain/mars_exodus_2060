ede5e2752e721a32d012e9f7e5aa2a664f449ed5
Hugues Romain <huguesromainpro@gmail.com>
2020-06-22 02:02:30 +0200

    init nav

--- src/exodus/apps/profile/index.tsx
+++ src/exodus/apps/profile/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { profileAppRouter } from "exodus/internal-router";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  display: flex;
+`;
 
 const ProfileApp = () => {
-    return <div>profile</div>;
+  return (
+    <>
+      <Nav>
+        <Link to={profileAppRouter.identity("1")}>Identité</Link>
+        <Link to={profileAppRouter.ticket("1")}>Billet</Link>
+      </Nav>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path={profileAppRouter.identity("1")}
+            render={() => <div />}
+          />
+          <Route
+            exact
+            path={profileAppRouter.ticket("1")}
+            render={() => <div />}
+          />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
-export default ProfileApp
+export default ProfileApp;
