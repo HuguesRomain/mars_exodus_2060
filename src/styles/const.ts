921191a7467cbace3531dc1158f019482539968d
Hugues Romain <huguesromainpro@gmail.com>
2020-06-23 00:21:53 +0200

    add color const

--- src/styles/const.ts
+++ src/styles/const.ts
@@ -1,3 +1,20 @@
 export const color = {
+  light: {
+    PureWhite: "#ffffff",
+    WhiteSmoke: "##F8F8F8",
+    Solitude: "##EFF3FF",
+    AliceBlue: "##F3F8FE",
+  },
+  medium: {
+    LinkWater: "##C1C6D7",
+  },
+  darker: {
+    BlackRussian: "##091124",
+    BlackPearl: "##072033",
+    LuckyPoint: "##292C59",
+  },
 
-}
+  Alizarin: "##E02637",
+  SunsetOrange: "##FC5252",
+  Lipstick: "##9B1D44",
+};
