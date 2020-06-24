fd46f6557e58a4ccd3a7dbb52ae1e024126eff5d
Hugues Romain <huguesromainpro@gmail.com>
2020-06-25 00:17:13 +0200

    [js] change routing for all apps

--- src/exodus/startup.tsx
+++ src/exodus/startup.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppContext } from "./main";
+import { AppContext } from "./oldMain";
 
 export const AppFrame = ({ children }: { children: JSX.Element }) => (
   <AppContext.Provider value={null}>{children}</AppContext.Provider>
